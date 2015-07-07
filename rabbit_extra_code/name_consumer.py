3e0745223c2f6abc9a0643d2a21e46001139c4fa
Grzegorz Parka <grzegorz.p@webinterpret.com>
2015-07-08 00:40:08 +0200

    rabbit presentation increment

--- rabbit_extra_code/name_consumer.py
+++ rabbit_extra_code/name_consumer.py
@@ -3,9 +3,16 @@ from kombu import Connection, Exchange, Queue
 
 name_queue = Queue('name', exchange='', routing_key='name')
 
+place = 1
+
 def show_name(body, message):
-    print body['name'], time.strftime('%X')
+    global place
+    try:
+        print place, '\t', time.strftime('%X'), '\t', body['name']
+    except TypeError, KeyError:
+        print place, '\t', time.strftime('%X'), '\t', body
     message.ack()
+    place += 1
     
 with Connection('amqp://guest:guest@localhost//') as conn:
     with conn.Consumer(name_queue, callbacks=[show_name]) as consumer:
